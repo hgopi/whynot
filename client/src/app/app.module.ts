eca892623f7a51d4899764493d1c0da5dd635ffb
Gopikrishnan <gopikrishnan.ind@gmail.com>
2019-05-01 17:57:37 +0530

    Completed header and started home

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,17 +4,30 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+
+import { SwiperModule, SwiperConfig, SwiperConfigInterface, SWIPER_CONFIG } from 'ngx-swiper-wrapper';
+
+const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
+  direction: 'horizontal',
+  slidesPerView: 1,
+  scrollbar: true
+};
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SwiperModule
   ],
-  providers: [],
+  providers: [
+    { provide: SWIPER_CONFIG, useValue: DEFAULT_SWIPER_CONFIG }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
